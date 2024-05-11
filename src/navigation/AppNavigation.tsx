a47db86e9f0def52d238dedc5a02692854e9f02b
Eman Atef Kamal Abdullah <eman@192.168.1.3>
2024-05-11 17:09:41 +0300

    remove commented code

--- src/navigation/AppNavigation.tsx
+++ src/navigation/AppNavigation.tsx
@@ -6,40 +6,22 @@ import {
   useNavigationContainerRef,
 } from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import moment from 'moment';
-import React, {useEffect} from 'react';
+import React from 'react';
 
 import {Routes} from '@/navigation';
 import {HomeScreen, CityDetails, HistoricalData} from '@/screens';
-// import {useStore} from '@/store';
-// import tw from '@/tw';
-
-// import {appLang} from '../utils';
 import {RootStackParamListType} from '.';
 
 const customThem = {
   ...DefaultTheme,
   colors: {
     ...DefaultTheme.colors,
-    // background: tw.color('white') as string,
   },
 };
 
 export const AppNavigation = () => {
-  // moment.locale(appLang);
-
   const Stack = createNativeStackNavigator<RootStackParamListType>();
   const navigationRef = useNavigationContainerRef();
-
-  // const storeLoaded = useStore(state => state._hasHydrated);
-
-  // useEffect(() => {
-  //   // storeLoaded && RNBootSplash.hide({fade: true}); // fade with custom duration
-  // }, [storeLoaded]);
-
-  // if (!storeLoaded) {
-  //   return null;
-  // }
 
   return (
     <NavigationContainer theme={customThem} ref={navigationRef}>
